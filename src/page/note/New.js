3cd7396717a7c27eb5060656c20684a60aab09aa
meparthka <meparthka.2005@gmail.com>
2022-06-05 11:48:49 +0530

    editor done

--- src/page/note/New.js
+++ src/page/note/New.js
@@ -1,8 +1,55 @@
+import React, { useEffect, useState } from 'react';
+import { EditorState, ContentState, convertToRaw } from 'draft-js';
+import htmlToDraft from 'html-to-draftjs';
+import draftToHtml from 'draftjs-to-html';
+import { Editor } from 'react-draft-wysiwyg';
+import 'react-draft-wysiwyg/dist/react-draft-wysiwyg.css';
+
 const New = () => {
-  
-  return(
+
+  const [editorState, setEditorState] = useState(
+    () => EditorState.createEmpty(),
+  );
+
+  useEffect(() => {
+
+    setTimeout(() => {
+      const blocksFromHTML = htmlToDraft(`
+      
+    `)
+
+      const content = ContentState.createFromBlockArray(
+        blocksFromHTML.contentBlocks,
+        blocksFromHTML.entityMap
+      );
+
+      setEditorState(() => EditorState.createWithContent(content))
+
+    }, 3000)
+  }, [])
+
+  const getHtml = () => {
+    const rawContentState = convertToRaw(editorState.getCurrentContent());
+    const markup = draftToHtml(rawContentState);
+    console.log(markup);
+  }
+
+  return (
     <>
-      <h1>New</h1>
+      <div className='con'>
+        <h1 className='sub-title'>New</h1>
+        <Editor
+          defaultEditorState={editorState}
+          editorState={editorState}
+          onEditorStateChange={setEditorState}
+          wrapperClassName="editor-wrapper-class"
+          editorClassName="editor-class"
+          toolbarClassName="editor-toolbar-class"
+        />
+      </div>
+      <button onClick={getHtml} className='btn btn-primary'>
+        Get Html
+      </button>
     </>
   )
 
