aaea7cde9358af2981d3a29b755525960ee912e8
meparthka <parthka.2005@gmail.com>
2022-06-10 11:42:15 +0530

    add tags

--- src/page/note/Read.js
+++ src/page/note/Read.js
@@ -22,10 +22,13 @@ const Read = () => {
     });
 
     if (res.data.status) {
-      setState({ title: res.data.title, html: res.data.html, edit: res.data.edit, fullname: res.data.fullname })
+      var options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
+      var datex = new Date(res.data.date);
+      var date = datex.toLocaleDateString("en-US", options)
+      setState({ title: res.data.title, tags: res.data.tags, visible: res.data.visible, date, found: true, html: res.data.html, edit: res.data.edit, fullname: res.data.fullname })
     } else {
       setState({
-        title: "Post Not Found!", html: `
+        title: "Post Not Found!", found: false, html: `
         <a class="link" href="/note">Click Here go to Home</a>
       ` })
     }
@@ -41,29 +44,58 @@ const Read = () => {
       <div className='con'>
         <div className='d-flex note-hade'>
           <p className='note-title'>{getState.title}</p>
-          <div className='d-flex'>
-            <Link className='m-y-auto d34jk' to={"/note/edit/" + id}><i class="bi bi-file-earmark-pdf pdf-icon"></i></Link>
-            {(() => {
-              if (getState.edit) {
-                return (
-                  <Link className='m-y-auto' to={"/note/edit/" + id}><i class="bi bi-pencil edit-icon"></i></Link>
+          {(() => {
+            if (getState.found === true) {
+              return (
+                <div className='d-flex'>
+                  <Link className='m-y-auto d34jk' to={"/note/edit/" + id}><i class="bi bi-file-earmark-pdf pdf-icon"></i></Link>
+                  {(() => {
+                    if (getState.edit) {
+                      return (
+                        <Link className='m-y-auto' to={"/note/edit/" + id}><i class="bi bi-pencil edit-icon"></i></Link>
+                      )
+                    } else {
+                      return (
+                        <>
+                          <div className='d-c-flex'>
+                            <div className='text-small-x m-t ujoasd'>
+                              Note By:
+                            </div>
+                            <b className='text-small m-b ujoasd'>
+                              {getState.fullname}
+                            </b>
+                          </div>
+                        </>
+                      )
+                    }
+                  })()}
+                </div>
+              )
+            }
+          })()}
+        </div>
+        <p className="text-small-l sdffds">
+          {getState.date + " "}
+          {(() => {
+            if (getState.edit && !getState.visible) {
+              return <i class="bi bi-eye color-gary"></i>
+            }
+          })()}
+        </p>
+        <div className='tags-y'>
+          {(() => {
+            if (getState.tags) {
+              let p = []
+              getState.tags.map((e) => {
+                p.push(
+                  <div className="tags-x">
+                    {e}
+                  </div>
                 )
-              } else {
-                return (
-                  <>
-                    <div className='d-c-flex'>
-                      <div className='text-small-x m-t'>
-                        Note By:
-                      </div>
-                      <b className='text-small m-b'>
-                        {getState.fullname}
-                      </b>
-                    </div>
-                  </>
-                )
-              }
-            })()}
-          </div>
+              })
+              return p
+            }
+          })()}
         </div>
         <hr />
         <div className='html-body' dangerouslySetInnerHTML={{ __html: getState.html }}>
